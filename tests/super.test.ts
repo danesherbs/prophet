7dc877d7014eac27523ed5af1f5184a1fb5c2ccd
Dane Sherburn <danesherbs@gmail.com>
2020-11-19 15:23:31 +1100

    Add super tests

--- tests/super.test.ts
+++ tests/super.test.ts
@@ -37,3 +37,31 @@ test('correct monthly net super contributions', () => {
 
     expect(superan.getMonthlyNetSuperContribution(120_000)).toEqual(120_000 * 0.125 * (1 - 0.15) / 12);
 });
+
+
+test('get transactions retrieves correct transactions', () => {
+    const tax = new Tax({
+        incomeTaxBrackets: new Array(),
+        superTaxRate: 0.15,
+        declared: new Array(),
+        paid: new Array()
+    });
+
+    const superan = new Super({
+        tax: tax,
+        transactions: [
+            [0, 1_000],
+            [4, 2_000],
+            [8, 4_000],
+        ],
+        interestRate: 0.1,
+        contributionRate: 0.125,
+    });
+
+    expect(superan.getTransactions())
+        .toEqual([
+            [0, 1_000],
+            [4, 2_000],
+            [8, 4_000],
+        ]);
+});
