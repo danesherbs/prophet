662800abde1a7756151436262db25b7190528158
Dane Sherburn <danesherbs@gmail.com>
2020-11-19 15:21:01 +1100

    Add expense tests

--- tests/expense.test.ts
+++ tests/expense.test.ts
@@ -23,3 +23,14 @@ test('monthly expenses increasing yearly', () => {
     expect(expense.getMonthlyAmount(12)).toBeCloseTo((120 * 52 / 12) * 1.03, 10);
     expect(expense.getMonthlyAmount(48)).toBeCloseTo((120 * 52 / 12) * Math.pow(1.03, 4), 10);
 });
+
+test('correct description', () => {
+    const expense = new Expense({
+        yearlyIncrease: 0.03,
+        weeklyAmount: 120,
+        description: "Living expenses",
+        initialTime: 0,
+    });
+
+    expect(expense.getDescription()).toEqual("Living expenses");
+});
