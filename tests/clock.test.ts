3f330b61c4cbabe103bd1f1921204efbe64e85a4
Dane Sherburn <danesherbs@gmail.com>
2020-11-20 21:58:30 +1100

    Refactor clock test

--- tests/clock.test.ts
+++ tests/clock.test.ts
@@ -1,9 +1,9 @@
 import { Clock } from "../src/clock";
 
 
-test('clock ticking correctly', () => {
-    const clock = new Clock(0);
+const clock = new Clock(0);
 
+test('clock ticking correctly', () => {
     expect(clock.getTime()).toEqual(0);
     expect(clock.tick().getTime()).toEqual(1);
 });
