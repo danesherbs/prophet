cbf65b81ab8481106a7014a727cc9b5c85a3d6c9
Dane Sherburn <danesherbs@gmail.com>
2020-11-20 21:57:58 +1100

    Refactor expense test

--- tests/expense.test.ts
+++ tests/expense.test.ts
@@ -1,36 +1,22 @@
 import { Expense } from "../src/expense";
 
 
-test('correct monthly expenses', () => {
-    const expense = new Expense({
-        yearlyIncrease: 0.03,
-        weeklyAmount: 120,
-        description: "Living expenses",
-        initialTime: 0,
-    });
+const expense = new Expense({
+    yearlyIncrease: 0.03,
+    weeklyAmount: 120,
+    description: "Living expenses",
+    initialTime: 0,
+});
 
+test('correct monthly expenses', () => {
     expect(expense.getMonthlyAmount(0)).toBeCloseTo(120 * 52 / 12, 10);
 });
 
 test('monthly expenses increasing yearly', () => {
-    const expense = new Expense({
-        yearlyIncrease: 0.03,
-        weeklyAmount: 120,
-        description: "Living expenses",
-        initialTime: 0,
-    });
-
     expect(expense.getMonthlyAmount(12)).toBeCloseTo((120 * 52 / 12) * 1.03, 10);
     expect(expense.getMonthlyAmount(48)).toBeCloseTo((120 * 52 / 12) * Math.pow(1.03, 4), 10);
 });
 
 test('correct description', () => {
-    const expense = new Expense({
-        yearlyIncrease: 0.03,
-        weeklyAmount: 120,
-        description: "Living expenses",
-        initialTime: 0,
-    });
-
     expect(expense.getDescription()).toEqual("Living expenses");
 });
