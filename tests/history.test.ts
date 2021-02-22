49d5f6f4f6821a21b730ec86658216987f079305
Dane Sherburn <danesherbs@gmail.com>
2021-02-22 13:08:59 +1100

    Add tests to history class

--- tests/history.test.ts
+++ tests/history.test.ts
@@ -107,6 +107,71 @@ test("bank balance is growing over time", () => {
   );
 });
 
+test("retrieval of house with start time returns correct time", () => {
+  expect(
+    history
+      .addEvent({
+        time: 25,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .getStart({ id: "new id" })
+  ).toEqual(25);
+});
+
+test("retrieval of house with no start time throws an error", () => {
+  expect(() => history.getStart({ id: "new id" })).toThrowError(RangeError);
+});
+
+test("retrieval house with end time returns correct time", () => {
+  expect(
+    history
+      .addEvent({
+        time: 25,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Sell,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .getEnd({ id: "new id" })
+  ).toEqual(50);
+});
+
+test("retrieval of house with no end time returns null", () => {
+  expect(
+    history
+      .addEvent({
+        time: 25,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .getEnd({ id: "new id" })
+  ).toEqual(null);
+});
+
 test("adding expense applies to first and last states in history", () => {
   const newExpense = new Expense({
     yearlyIncrease: 0.1,
@@ -148,4 +213,216 @@ test("adding expense applies to first and last states in history", () => {
       .getState(120)
       .getExpenses()
   ).toEqual({ ...expenses, "new expense": newExpense });
+});
+
+test("adding event at time adds correct asset", () => {
+  expect(
+    history
+      .addEvent({
+        time: 0,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .getState(30)
+      .getHouses()
+  ).toEqual({ ...houses, "new id": houses["main"] });
+});
+
+test("adding and removing event doesn't alter original state", () => {
+  expect(
+    history
+      .addEvent({
+        time: 25,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .removeEvent({
+        time: 25,
+        id: "new id",
+        action: Action.Add,
+      })
+      .getState(25)
+  ).toEqual(history.getState(25));
+});
+
+test("adding and removing multiple events doesn't alter original state", () => {
+  expect(
+    history
+      .addEvent({
+        time: 25,
+        event: {
+          action: Action.Add,
+          item: {
+            id: "new stock",
+            object: stocks["ihvv"],
+          },
+        },
+      })
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new house",
+            object: houses["main"],
+          },
+        },
+      })
+      .removeEvent({
+        time: 25,
+        id: "new stock",
+        action: Action.Add,
+      })
+      .removeEvent({
+        time: 50,
+        id: "new house",
+        action: Action.Buy,
+      })
+      .getState(100)
+  ).toEqual(history.getState(100));
+});
+
+test("removing events are idempotent", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .removeEvent({
+        time: 50,
+        id: "new id",
+        action: Action.Add,
+      })
+      .removeEvent({
+        time: 50,
+        id: "new id",
+        action: Action.Buy,
+      })
+      .getState(100)
+  ).toEqual(history.getState(100));
+});
+
+test("removing non-existent event has no effect", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .removeEvent({
+        time: 50,
+        id: "new id",
+        action: Action.Sell,
+      })
+      .removeEvent({
+        time: 25,
+        id: "new id",
+        action: Action.Buy,
+      })
+      .getState(100)
+  ).toEqual(history.getState(100));
+});
+
+test("removing events are idempotent", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .removeEvent({
+        time: 50,
+        id: "new id",
+        action: Action.Add,
+      })
+      .removeEvent({
+        time: 50,
+        id: "new id",
+        action: Action.Buy,
+      })
+      .getState(100)
+  ).toEqual(history.getState(100));
+});
+
+test("setting start of existing house sets to correct value", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .setStart({ time: 75, id: "new id", item: houses["main"] })
+      .getStart({ id: "new id" })
+  ).toEqual(75);
+});
+
+test("setting end of existing house sets to correct value", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .setEnd({ time: 55, id: "new id", item: houses["main"] })
+      .getEnd({ id: "new id" })
+  ).toEqual(55);
+});
+
+test("deleting end of existing house sets end to null", () => {
+  expect(
+    history
+      .addEvent({
+        time: 50,
+        event: {
+          action: Action.Buy,
+          item: {
+            id: "new id",
+            object: houses["main"],
+          },
+        },
+      })
+      .setEnd({ time: null, id: "new id", item: houses["main"] })
+      .getEnd({ id: "new id" })
+  ).toEqual(null);
 });
